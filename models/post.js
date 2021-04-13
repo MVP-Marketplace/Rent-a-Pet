277612e08ab8c4f05503821f1fd06de8bce8ff4b
Traci Levine <62347318+tracilevine@users.noreply.github.com>
2021-04-13 12:51:17 -0400

    Update post.js

--- models/post.js
+++ models/post.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const postSchema = mongoose.Schema(
   {
     user_id: { type: String, required: true },
-    media: { type: String, requied: true },
+    media: { type: String, required: true },
     total_likes: { type: Number, default: 0 },
     total_comments: { type: Number, default: 0 },
   },
